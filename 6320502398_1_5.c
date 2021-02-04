144eb5ed1d0802ada87cf82f2bce3b781ba9dd41
ntwww <nattawut.rod@ku.th>
2021-02-04 17:16:15 +0700

    Update 6320502398_1_5.c

--- 6320502398_1_5.c
+++ 6320502398_1_5.c
@@ -1,18 +1,22 @@
-#include<Stdio.h>
+#include <stdio.h>
 int main()
 {
-int n;
-scanf("%d",&n);
-int a[n],i,max,b;
-for(i=0;i<n;i++)
-{
-scanf("%d",&a[i]);
-if(max<a[i])
-{
-    max=a[i];
-    b=i;
-}
-}
-printf("%d %d",b+1,max);
+    int n,i,max=0,b=0;
+    scanf ("%d",&n);
+    int a[n];
+    for(i=1;i<=n;i++)
+    {
+        scanf ("%d",&a[i]);
+    }
+    for(i=1;i<=n;i++)
+    {
+        if(max<a[i])
+        {
+            max=a[i];
+            b=i;
+        }
+    }
+    printf ("%d %d",b,max);
+
 }
 
